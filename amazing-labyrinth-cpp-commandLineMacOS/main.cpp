a318a62b3ae8da9200b43d4c9079d3f0861db2e3
Kevin Miller <kmillergit@outlook.com>
2021-05-31 12:49:50 -0700

    basic class layout

--- amazing-labyrinth-cpp-commandLineMacOS/main.cpp
+++ amazing-labyrinth-cpp-commandLineMacOS/main.cpp
@@ -1,14 +1,56 @@
 //
 //  main.cpp
-//  amazing-labyrinth-cpp-commandLineMacOS
+//  kevins-cpp-command-line-project
 //
 //  Created by Kevin Miller on 5/31/21.
 //
 
 #include <iostream>
+using namespace std;
+
+const int player_count = 4;
+int current_player = 1;
+
+class Side {
+public:
+    
+private:
+    bool isOpen;
+};
+
+class Tile {
+public:
+    bool containsPlayer;
+    bool containsItem;
+    string item;
+    
+    bool getIsMovable() {
+        return isMovable;
+    }
+    void setIsMovable(bool movable) {
+        isMovable = movable;
+    }
+private:
+    bool isMovable;
+    Side sides[4];
+};
+
+
+class Board {
+public:
+
+private:
+    Tile board[8][8];
+};
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "Hello, Player!\n" << endl;
+    
+    Board* labyrinth = new Board;
+    
+    
 }
