f3490b3cb6d9ff7e74d696704809ae52e91cd5ff
Kevin Miller <kmillergit@outlook.com>
2021-06-01 23:36:28 -0700

    more classes

--- amazing-labyrinth-cpp-commandLineMacOS/main.cpp
+++ amazing-labyrinth-cpp-commandLineMacOS/main.cpp
@@ -6,7 +6,10 @@
 //
 
 #include <iostream>
+#include <cstdlib> // for rng
+
 using namespace std;
+
 
 const int player_count = 4;
 int current_player = 1;
@@ -16,10 +19,32 @@ public:
     
 private:
     bool isOpen;
+    
 };
+
 
 class Tile {
 public:
+    Tile() {
+        _sides = new Side[4];
+        
+        bool hasOneSideOpen = false;
+        while (hasOneSideOpen == false) {
+            for (int i = 0; i < 4; i++) {
+                bool isOpen;
+                if ( (rand() % 2) == 1) {
+                    isOpen = true;
+                    hasOneSideOpen = true;
+                    cout << isOpen << endl;
+                }
+                else {
+                    isOpen = false;
+                    cout << isOpen << endl;
+                }
+            }
+        }
+    }
+
     bool containsPlayer;
     bool containsItem;
     string item;
@@ -32,25 +57,60 @@ public:
     }
 private:
     bool isMovable;
-    Side sides[4];
+    Side* _sides;
 };
 
 
 class Board {
 public:
-
+    Board() {
+        _board = new Tile*[8];
+        for (int i = 0; i < 8; i++) {
+            _board[i] = new Tile[8];
+        }
+    }
+    
+    Tile** getBoard(void) {
+        cout << "get board function" << endl;
+        cout << _board;
+        return _board;
+    }
+    
 private:
-    Tile board[8][8];
+    Tile** _board;
 };
 
 
+class Treasure {
+private:
+    string name;
+public:
+    string getName(void) {
+        return name;
+    }
+    void setName(string newName) {
+        name = newName;
+    }
+};
+
+
+class Card {
+private:
+    Treasure depictedTreasure;
+public:
+    Treasure getDepictedTreasure() {
+        return depictedTreasure;
+    }
+};
 
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, Player!\n" << endl;
+    cout << "Hello, Player!" << endl;
+    cout << "Here is the board." << endl;
     
     Board* labyrinth = new Board;
+    labyrinth->getBoard();
     
-    
+    return 0;
 }
